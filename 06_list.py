69873c792a39ccb349b3f8b541b7506d9da33410
Karan Negi <knegi2003@outlook.com>
2025-03-14 12:58:44 +0530

    this is all about list

--- 06_list.py
+++ 06_list.py
@@ -27,14 +27,36 @@ print(len(user_list)) #to find the length of list.
 
 #------------------------Sorting of list------------------------------
 
-user_list.sort()
+user_list.sort() #this will sort the whole list
 print(user_list)
 
-user_list.sort(reverse=True)
+user_list.sort(reverse=True) #this will sort the list in reverse order.
 print(user_list)
 
+user_list.reverse() #will reverse all the values with in a list
+print (user_list)
 
+user_list.pop() #Pop remove last/recent added value from the list
+print(user_list)
+#diff btw Remove&Pop when we use remove with in print it will give us eror but Pop provide us the value as well as we can use pop witn in a variable to retrive something 
+'''
+exampple of Pop 
+removed_value = user_list.pop()
+print(removed_value)
+'''
+user_list.pop(2)
+print(user_list)
 
+#-----------Numeric list ---------------------
 
+marks = [23, 3, 343, 34, 65, 87, 843, 90, 243, 13]
+print(marks)
+print(min(marks))
+print(max(marks))
+print(sum(marks))
+
+#----List--
+
+Mix_list = ['karan', 23, 43.45]
 
 
